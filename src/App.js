1ce92d116bd5e97788fc0d798205a8dad35aba23
James Thang <baokieuha@gmail.com>
2019-07-10 23:04:06 +0700

    update with shop page and image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/homepage'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
