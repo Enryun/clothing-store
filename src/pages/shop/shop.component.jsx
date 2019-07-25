0f8df32ee7e930c2cebb333d04a352f95d772033
James Thang <baokieuha@gmail.com>
2019-07-26 02:01:10 +0700

    adding Category Page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import {selectShopCollections} from '../../redux/shop/shop.selector';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
+import {Route} from 'react-router-dom';
+import CollectionsPage from '../collection/collection.component';
+import CollectionsOverview from '../../components/collection-overview/collection-overview.component';
+ 
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-const ShopPage = ({collections}) => (
+
+const ShopPage = ({match}) => (
 
     <div className='shop-page'>
-        {collections.map(({id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionsPage} />
     </div>
 
 )
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectShopCollections
-})
+
            
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
