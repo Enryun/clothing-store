691e9f4957b00cc944160f5af43349a037fe043f
James Thang <baokieuha@gmail.com>
2019-07-26 16:02:44 +0700

    finish /shop/:colletion display, improving Data Query

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -4,11 +4,20 @@ import {selectCollection} from '../../redux/shop/shop.selector'
 import CollectionItem from '../../components/collection-item/collection-item.component';
 import './collection.styles.scss';
 
-const CollectionsPage = ({match}) => (
-    <div className='collection'>
-        <h2>Category Page</h2>
-    </div>
-)
+const CollectionsPage = ({collection, match}) => {
+
+    const {title, items} = collection;
+    return (
+        <div className='collection-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+                {
+                    items.map(item => (<CollectionItem key={item.id} item={item} />))
+                }
+            </div>
+        </div>
+    ) 
+}
 
 const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
