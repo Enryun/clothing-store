212cc81119a9284229cec4b59903af878c0de007
James Thang <baokieuha@gmail.com>
2019-08-15 09:18:44 +0700

    bring in BackEnd for Stripe Payment

--- client/src/components/stripe/stripe-button.component.jsx
+++ client/src/components/stripe/stripe-button.component.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_fQwLyWLoQ5P1MF8jx1UEZGUN0005QgoWvt';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Success')
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => alert('Payment Success'))
+          .catch(error => {
+              console.log('Payment error: ', JSON.parse(error));
+              alert('There is the issue with your payment. Please make sure you use the provided Credit Card');
+          })
     }
 
     return (
